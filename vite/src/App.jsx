5217b8255e1bf91c1a417b6a861e61a2303dd052
kausikimaddula <saikausikimaddula80@gmail.com>
2025-03-05 14:46:12 +0530

    committed

---
+++ vite/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 id="hii">NAME:MADDULA SAI KAUSIKI</h1>
+        <h3 id='hello'>Email:saikausikimaddula80@gmail.com</h3>
+        <h3 id='bye'>ph.no:8374695895</h3>
+   
+      </div>
+    
+    
+    </>
+  )
+}
+
+export default App
